baedda035920d393a3d05a1cb11bf8e421041ac7
DavidJoao <davidsandoval596@gmail.com>
2023-03-05 22:13:16 -0800

    Create sorting view
    Still need to fix bug

--- vite-front/src/App.jsx
+++ vite-front/src/App.jsx
@@ -1,10 +1,50 @@
 import './App.css'
+import {data} from './assets/data'
+import { useState } from 'react';
 
 function App() {
 
+	const [myData, setMyData] = useState(data)
+
+	const insertionSort = (arr) => {
+		let newArr = []
+		for (let i = 1; i < arr.length; i++){
+			 for (let j = i; j > 0; j--){
+				  if (arr[j].objVal < arr[j - 1].objVal){
+						let temp = arr[j].objVal;
+						let tempText = arr[j].text;
+
+						arr[j].objVal = arr[j - 1].objVal
+						arr[j].text = arr[j - 1].text
+
+						arr[j - 1].objVal = temp
+						arr[j - 1].text = tempText
+
+						newArr.push(arr[i - 1].objVal)
+				  } else {
+						break;
+				  }
+			 }
+			}
+		setMyData(newArr)
+		console.log(arr)
+		return arr
+  }
+
+	const handleClick = () => {
+		insertionSort(myData)
+	}
+
   return (
-    <div className="App">
-      
+    <div style={{ display:"flex", flexDirection:'column', alignItems:'center', justifyContent:"center", height:"100vh" }}>
+      <button style={{ marginBottom:'25px' }} onClick={handleClick}>Sort</button>
+      <div style={{ border:'1px solid white', width: '80%', height:"50%", display:"flex", flexDirection:"row", justifyContent:'center', alignItems:'center' }} >
+			{myData.map((item) => {
+				return(
+					<div key={item.objVal || item} style={{ border:'1px solid white', margin:"10px", height:"50px", width:"50px", display:"flex", justifyContent:"center", alignItems:"center", color:"black", backgroundColor:'white' }}>{item.objVal || item}</div>
+				)
+			})}
+      </div>
     </div>
   )
 }
---
+++ vite-front/src/assets/data.js
@@ -0,0 +1,35 @@
+export let data =
+[
+    {
+        text: '1',
+        objVal: 1
+    },
+    {
+        text: '23',
+        objVal: 23
+    },
+    {
+        text: '3',
+        objVal: 3
+    },
+    {
+        text: '256',
+        objVal: 256
+    },
+    {
+        text: '7',
+        objVal: 7
+    },
+    {
+        text: '18',
+        objVal: 18
+    },
+    {
+        text: '63',
+        objVal: 63
+    },
+    {
+        text: '14',
+        objVal: 14
+    },
+]
